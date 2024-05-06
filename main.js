165e9b7624bb2339ee6d0ed313b1f6cfc1601446
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-05-06 23:15:25 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+//MAKING TABLES OF 2 TO 5 USING FOR LOOP
+console.log("TABLE OF 2");
+var num1 = 2;
+for (var i = 1; i <= 10; i++) {
+    console.log("".concat(num1, " * ").concat(i, " = ").concat(num1 * i));
+}
+console.log("TABLE OF 3");
+var num2 = 3;
+for (var i = 1; i <= 10; i++) {
+    console.log("".concat(num2, " * ").concat(i, " = ").concat(num2 * i));
+}
+console.log("TABLE OF 4");
+var num3 = 4;
+for (var i = 1; i <= 10; i++) {
+    console.log("".concat(num3, " * ").concat(i, " = ").concat(num3 * i));
+}
+console.log("TABLE OF 5");
+var num4 = 5;
+for (var i = 1; i <= 10; i++) {
+    console.log("".concat(num4, " * ").concat(i, " = ").concat(num4 * i));
+}
---
+++ main.ts
@@ -0,0 +1,29 @@
+//MAKING TABLES OF 2 TO 5 USING FOR LOOP
+console.log("TABLE OF 2")
+let num1:number= 2
+
+for(let i:number=1; i<=10 ; i++){
+    console.log(`${num1} * ${i} = ${num1*i}`)
+}
+
+console.log("TABLE OF 3")
+let num2:number= 3
+
+for(let i:number=1; i<=10 ; i++){
+    console.log(`${num2} * ${i} = ${num2*i}`)
+}
+
+console.log("TABLE OF 4")
+let num3:number= 4
+
+for(let i:number=1; i<=10 ; i++){
+    console.log(`${num3} * ${i} = ${num3*i}`)
+}
+
+console.log("TABLE OF 5")
+let num4:number= 5
+
+for(let i:number=1; i<=10 ; i++){
+    console.log(`${num4} * ${i} = ${num4*i}`)
+}
+
